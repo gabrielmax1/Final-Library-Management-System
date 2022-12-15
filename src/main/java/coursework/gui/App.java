bf34eaa03693c4dbc306970a3ca4b32d660dcb87
Gab <cotigliani@gmail.com>
2022-12-15 22:34:58 +0000

    Last commit and push

--- src/main/java/coursework/gui/App.java
+++ src/main/java/coursework/gui/App.java
@@ -2,6 +2,7 @@ package coursework.gui;
 // This package is a collection of classes used for creating graphical user interface (GUI) components for Java programs.
 
 // THIS REPOSITORY IS BASED ON LAB9 OF THE MODULE CREATED BY RAFAEL MARTINEZ TORRES.
+// Version Control https://github.com/gabrielmax1/Final-Library-Management-System.git
 import javax.swing.*;
 
 // This is the critical Class in the View which will instantiate the MainPanel class using setContentPane from javax.swing
